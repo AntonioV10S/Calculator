6369f3f595d0412ea7fc5ce04d2628c4690c7f40
antoniov10s <marcoantoniovesa2003@gmail.com>
2023-10-23 12:23:13 -0500

    Diseno

---
+++ Script.js
@@ -0,0 +1,8 @@
+var numero1 = document.getElementById("n1");
+var numero2 = document.getElementById("n2");
+var resul = document.getElementById("Resultado")
+
+function Sumar()
+{
+    numero1.textContent()
+}
